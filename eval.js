3eeb4cf6f1ebd14c9299d80e78997fc8093a7155
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2020-08-23 19:16:01 +1200

    some more interesting evals

--- eval.js
+++ eval.js
@@ -1,4 +1,5 @@
 var types = require('./types')
+var inspect = require('util').inspect
 
 function isPrimitive (node) {
   return (
@@ -11,21 +12,52 @@ function isPrimitive (node) {
 
 function call (fn, args, scope) {
   //eval with built in function
+  if(!fn) throw new Error('cannot call undefined')
   if('function' === typeof fn) {
-    if(args.length !== fn.length)
-      throw new Error('incorrect number of arguments for:'+fn)
+    if(args.length !== fn.length) {
+      throw new Error('incorrect number of arguments for:'+fn+', got:'+args)
+    }
     return fn.apply(null, args)
   }
   
   if(args.length !== fn.args.length)
-    throw new Error('incorrect number of arguments for:'+fn)
+    throw new Error('incorrect number of arguments for:'+inspect(fn, {colors:true})+', got:'+args)
   var _scope = {__proto__: scope}
   for(var i = 0; i < fn.args.length; i++)
     _scope[fn.args[i].description] = args[i]
+  if(fn.name)
+    _scope[fn.name.description] = fn 
   return ev(fn.body, _scope)
 }
 
+function ev_ab(node, scope) {
+  var v = ev(node, scope)
+  if(typeof v !== 'boolean') throw new Error('expected boolean')
+  return v
+}
+
+var True = {type: types.boolean, value: true}
+var False = {type: types.boolean, value: true}
+
+function ev_if(a,b,c, scope) {
+    if(ev_ab(a, scope) === true) return ev(b, scope)
+    else return ev(c, scope)  
+}
+
+function bind (fn, scope, name) {
+  return {
+    type: types.fun,
+    args: fn.args,
+    body: fn.body,
+    scope: scope,
+    name: name || null
+  }
+}
+
 function ev (node, scope) {
+  if(!node) throw new Error('null node')
+    if(!scope) throw new Error('missing scope')
+
   if(isPrimitive(node)) return node.value
   
   if(node.type === types.symbol) return scope[node.value.description]
@@ -35,6 +67,10 @@ function ev (node, scope) {
     var fn = ev(node.value, scope)
     return call(fn, node.args.map(a => ev(a, scope)), scope)
   }
+
+  if(node.type === types.if)  return ev_if(node.left, node.mid, node.right, scope)
+  if(node.type === types.and) return ev_if(node.left, node.right, False, scope)
+  if(node.type === types.or)  return ev_if(node.left, True, node.right, scope)
   
   if(node.type === types.object) {
     var obj = {}
@@ -49,9 +85,19 @@ function ev (node, scope) {
       ary[i] = ev(node.value[i], scope)
     return ary
   }
-    
+  
+  if(node.type === types.def) {
+    //handle function defs specially, to enable recursion
+    if(node.right.type === types.fun) {
+      var name = node.left.value
+      return scope[name.description] = bind(node.right, scope, name)
+    }
+    else
+      return scope[name.description] = ev(node.right, scope)
+  }
+
   if(node.type === types.fun)
-    return {type: types.fun, args: node.args, body: node.body, scope: scope}
+    return bind(node, scope)
 }
 
 module.exports = ev
--- test/eval.js
+++ test/eval.js
@@ -3,21 +3,46 @@ var parse = require('../parse')()
 var ev = require('../eval')
 
 var inputs = [
-  'plus(1 2)',
-  '{a b; plus(a b)}(3 4)'
+  'add(1 2)',
+  '{a b; add(a b)}(3 4)',
+  'true ? 10 ; -10',
+  'true & true & false',
+  'true | false',
+  '{x;eq(and(x 1) 1) ? true ; false}(1)',
+  '{x;eq(and(x 1) 1) ? true ; false}(4)',
+//  '{;{;}}()'
+//  'fac:{n; eq(n 1) ? 1 ; mul(n fac(add(-1 n)) )}'
+  '{;fac:{n; eq(n 1) ? 1 ; mul(n fac(add(-1 n)))}}()(1)',
+  '{;fac:{n; eq(n 1) ? 1 ; mul(n fac(add(-1 n)))}}()(7)'
 ]
 
 var outputs = [
   3,
-  7
+  7,
+  10,
+  false,
+  true,
+  true,
+  false,
+  1,
+  1*2*3*4*5*6*7
 ]
 
 var scope = {
-  plus: function (a, b) { return a + b }
+  add: function (a, b) { return a + b },
+  and: function (a, b) { return a & b },
+  mul: function (a, b) { return a * b },
+  eq: function (a, b) { return a === b },
+  
 }
 
 for(var i = 0; i < inputs.length; i++) {
-  var v = ev(parse(inputs[i]), scope)
-  console.log(v)
-  assert.equal(v, outputs[i])
+  if(inputs[i]) {
+    var ast = parse(inputs[i])
+    console.log("SRC", inputs[i])
+    console.log("AST", ast)
+    var v = ev(ast, scope)
+    console.log(v)
+    assert.equal(v, outputs[i])
+  }
 }
