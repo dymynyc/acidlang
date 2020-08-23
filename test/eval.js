f465692e56aeee46177982e2f716be2f3a47997c
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2020-08-23 21:24:05 +1200

    tidy

--- test/eval.js
+++ test/eval.js
@@ -1,19 +1,18 @@
 var assert = require('assert')
 var parse = require('../parse')()
 var ev = require('../eval')
-
+var inspect = require('util').inspect
 var inputs = [
   'add(1 2)',
   '{a b; add(a b)}(3 4)',
   'true ? 10 ; -10',
   'true & true & false',
   'true | false',
-  '{x;eq(and(x 1) 1) ? true ; false}(1)',
-  '{x;eq(and(x 1) 1) ? true ; false}(4)',
-//  '{;{;}}()'
-//  'fac:{n; eq(n 1) ? 1 ; mul(n fac(add(-1 n)) )}'
+  '{x; eq(and(x 1) 1) ? true ; false}(1)',
+  '{x; eq(and(x 1) 1) ? true ; false}(4)',
   '{;fac:{n; eq(n 1) ? 1 ; mul(n fac(add(-1 n)))}}()(1)',
-  '{;fac:{n; eq(n 1) ? 1 ; mul(n fac(add(-1 n)))}}()(7)'
+  '{;fac:{n; eq(n 1) ? 1 ; mul(n fac(add(-1 n)))}}()(7)',
+  '{; sq:{x m; gt(x m) ? x ; sq(mul(x x) m) } }()(2 10000)'
 ]
 
 var outputs = [
@@ -25,7 +24,8 @@ var outputs = [
   true,
   false,
   1,
-  1*2*3*4*5*6*7
+  1*2*3*4*5*6*7,
+  65536
 ]
 
 var scope = {
@@ -33,14 +33,14 @@ var scope = {
   and: function (a, b) { return a & b },
   mul: function (a, b) { return a * b },
   eq: function (a, b) { return a === b },
-  
+  gt: function (a, b) { return a > b }  
 }
 
 for(var i = 0; i < inputs.length; i++) {
   if(inputs[i]) {
     var ast = parse(inputs[i])
     console.log("SRC", inputs[i])
-    console.log("AST", ast)
+    console.log("AST", inspect(ast, {colors:true, depth:1000}))
     var v = ev(ast, scope)
     console.log(v)
     assert.equal(v, outputs[i])
