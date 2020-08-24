52a570185a4ff61144b1d0c635da4e54df4e9364
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2020-08-24 23:51:50 +1200

    tidy

--- compile-js.js
+++ compile-js.js
@@ -5,35 +5,36 @@ function args (args) {
 }
 
 function compile(node) {
-  if(node.type === types.boolean)
+  var type = node.type
+  if(type === types.boolean)
     return node.value.toString()
-  if(node.type === types.number)
+  if(type === types.number)
     return node.value.toString()
-  if(node.type === types.string)
+  if(type === types.string)
     return JSON.stringify(node.value.toString())
-  if(node.type === types.symbol)
+  if(type === types.symbol)
     return node.value.description
-  if(node.type === types.nil)
+  if(type === types.nil)
     return 'null'
-  if(node.type === types.if)
+  if(type === types.if)
     return '(' + compile(node.left) + ' ? ' + compile(node.mid) + ' : ' + compile(node.right) + ')'
-  if(node.type === types.and)
+  if(type === types.and)
     return '(' + compile(node.left) + ' ? ' + compile(node.right) + ' : false)'
-  if(node.type === types.or)
+  if(type === types.or)
     return '(' + compile(node.left) + ' ? true : ' + compile(node.right) + ')' 
-  if(node.type === types.fun) {
+  if(type === types.fun) {
     if(node.name)
       return '(function '+node.name.description+' (' + args(node.args)+'){return '+compile(node.body)+'})'
     else
       return '((' + args(node.args).trim() + ') => ' + compile(node.body) + ')'
   }
-  if(node.type === types.object)
-    return '{'+Object.keys(node.value).map(k => k + ':' + compile(node.value[k])).join(', ')+'}'
-  if(node.type === types.array)
-    return '['+node.value.map(v => compile(v)).join(', ')+']'
-  if(node.type === types.set || node.type === types.def)
+  if(type === types.object)
+    return 'Object.seal({'+Object.keys(node.value).map(k => k + ':' + compile(node.value[k])).join(', ')+'})'
+  if(type === types.array)
+    return '(['+node.value.map(v => compile(v)).join(', ')+'])'
+  if(type === types.set || node.type === types.def)
     return '('+node.left.value.description + '='+compile(node.right)+')'
-  if(node.type === types.call) {
+  if(type === types.call) {
     return compile(node.value) + '(' + args(node.args) + ')'
   }
 }
