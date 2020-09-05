7229f17a9f6b98e74577176839cd46650c3429a3
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2020-09-06 11:23:06 +1200

    since accessing a non property is an error, we need a built in to check it

--- env.js
+++ env.js
@@ -18,6 +18,10 @@ module.exports = Object.freeze({
   createSymbol: function (s) {
     return $(s)
   },
+  has: function (object, key) {
+    console.log(["HAS", object, key])
+    return object[key] != undefined
+  },
   add: function (a, b) { return a + b },
   and: function (a, b) { return a & b },
   mul: function (a, b) { return a * b },
