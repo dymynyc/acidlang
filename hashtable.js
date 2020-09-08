9ccd89cf5f897dfb05eb65f7161e9ea2b5a7abe2
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2020-09-08 21:28:54 +1200

    hashtable as wrapper around map

---
+++ hashtable.js
@@ -0,0 +1,24 @@
+function toString(k) {
+  return k = 'symbol' === typeof k ? k.description : k
+}
+
+module.exports = function (parentMap) {
+  var map = new Map()
+  return {
+    has: function (k) {
+      k = toString(k)
+      return map.has(k) ? true : parentMap != null ? parentMap.get(k) : false      
+    },
+    hasOwn: function (k) {
+      return map.has(toString(k))
+    },
+    get: function (x) {
+      x = toString(x)
+      return map.has(x) ? map.get(x) : parentMap != null ? parentMap.get(x) : null
+    },
+    set: function (k, v) {
+      map.set(toString(k), v)
+      return v
+    }
+  }
+}
