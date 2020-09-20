c3706a5673b1f52fdaea99cdc1a13124f229df47
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2020-09-20 19:08:28 +1200

    new hashtable methods needed

--- hashtable.js
+++ hashtable.js
@@ -1,3 +1,4 @@
+var $ = require('./symbols')
 function toString(k) {
   return k = 'symbol' === typeof k ? k.description : k
 }
@@ -13,13 +14,23 @@ module.exports = function (parentMap) {
     hasOwn: function (k) {
       return map.has(toString(k))
     },
+    //1 if on this map, 2 if on parent map, 3 on parent parent map, etc
+    hasDepth: function (k) {
+      return map.has(toString(k)) ? 1 :
+             parentMap !=null ? 0 != (d=parentMap.hasDepth(toString(k))) ? d+1 : 0 : 0
+    },
     get: function (x) {
       x = toString(x)
       return map.has(x) ? map.get(x) : parentMap != null ? parentMap.get(x) : null
     },
     set: function (k, v) {
+      if(null == v)
+        throw new Error('cannot set hashtable to nullish:'+toString(k)+'='+v)
       map.set(toString(k), v)
       return v
+    },
+    entries: function () {
+      return Array.from(map.entries()).map((kv) => ({key:$(kv[0]), value:kv[1]}))
     }
   }
 }
