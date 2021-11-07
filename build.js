b44f5d50671bba162e8ef29295bf610f41703edd
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2021-11-08 11:18:17 +1300

    handle require into node_modules

--- build.js
+++ build.js
@@ -7,7 +7,17 @@ var {inspect} = require('util')
 var resolve = require('./resolve')('node_modules', '.al', JSON.parse, "package.json")
 
 function toRelative(s) {
+  console.log("TO_RELATIVE", s)
+  var nm = /^(?:\.\.\/)+node_modules\//.exec(s)
+  console.error(nm)
+  if(nm) return s.substring(nm[0].length)
+
   return (/^\.\./.test(s) ? '' : './') + path.normalize('./'+ s)
+}
+
+//the file name before the extention
+function prename (rel) {
+  return rel.substring(0, rel.length - path.extname(rel).length)
 }
 
 module.exports = function (parse, compile) {
@@ -16,16 +26,23 @@ module.exports = function (parse, compile) {
     function build(module, from) {
       var used = {}
       var target = resolve(module, from)
+    console.error("TARGET", module, '->',  target)
       var rel = path.relative(context, target)
-      var outfile = path.join(output, path.relative(context, rel.substring(0, rel.length - path.extname(rel).length) + '.js'))
+      var outfile = path.join(output, path.relative(context, prename(rel) + '.js'))
+      console.error("REQUIRE("+JSON.stringify(module)+', '+JSON.stringify(from)+')')
+      var desc = JSON.stringify({module, from, target})
+      //if we directly resolved a js file, then just include the require
+      //do not build.
       if('.js'  === path.extname(target)) {
-        return 'require('+JSON.stringify(toRelative(path.relative(path.dirname(outfile), target)))+')/*direct js*/'
+        return 'require('+JSON.stringify(toRelative(path.relative(path.dirname(outfile), target)))+')/*direct js:'+desc+'*/'
       }
       var relfrom = path.relative(context, from) //where the parent module will end up
       var outfrom = path.join(output, relfrom)
       var outrel = rel.substring(0, rel.length - path.extname(rel).length) + '.js'
       mkdirp.sync(path.dirname(outfile))
-      if(sources[target]) return 'require('+JSON.stringify(toRelative(outrel))+')/*compiled al*/'
+      //if the module required is already built (in sources) then just return require string, don't build it.
+
+      if(sources[target]) return 'require('+JSON.stringify(toRelative(outrel))+')/*compiled al:'+desc+'*/'
       sources[target] = true
 
       var scope = {}
@@ -58,8 +75,10 @@ module.exports = function (parse, compile) {
       for(var k in used)
         builtins += 'var '+k+' = '+env[k].toString()+';\n'
 
+      //else write file and return require string
+
       fs.writeFileSync(outfile, builtins + 'module.exports = ' + out)
-      return 'require('+JSON.stringify(toRelative(outrel))+')/*compiled*/'
+      return 'require('+JSON.stringify(toRelative(outrel))+')/*compiled al (first):'+desc+'*/'
     }
 
     if(Array.isArray(entry))
